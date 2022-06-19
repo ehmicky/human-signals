ee327dd64e0426803342d507fa1eb4ff559a6867
ehmicky <ehmicky@gmail.com>
2022-06-19 21:47:13 +0200

    Improve types

---
+++ src/main.test-d.ts
@@ -0,0 +1,38 @@
+import {
+  signalsByName,
+  signalsByNumber,
+  Signal,
+  SignalNumber,
+  SignalName,
+  SignalAction,
+  SignalStandard,
+} from 'human-signals'
+import { expectAssignable, expectNotAssignable, expectType } from 'tsd'
+
+expectType<Signal>(signalsByName.SIGINT)
+expectType<Signal>(signalsByNumber[1])
+
+const { name, number, description, supported, action, forced, standard } =
+  signalsByName.SIGINT
+expectType<SignalName>(name)
+expectType<SignalNumber>(number)
+expectType<string>(description)
+expectType<boolean>(supported)
+expectType<SignalAction>(action)
+expectType<boolean>(forced)
+expectType<SignalStandard>(standard)
+
+expectAssignable<SignalName>('SIGINT')
+expectNotAssignable<SignalName>(true)
+expectNotAssignable<SignalName>('INT')
+
+expectAssignable<SignalNumber>(1)
+expectNotAssignable<SignalNumber>('1')
+
+expectAssignable<SignalAction>('terminate')
+expectNotAssignable<SignalAction>(true)
+expectNotAssignable<SignalAction>('unknown')
+
+expectAssignable<SignalStandard>('ansi')
+expectNotAssignable<SignalStandard>(true)
+expectNotAssignable<SignalStandard>('unknown')
